9bd385267c2811634aa975e1cbc6a79d10520d24
aldiwira <aldiwira.aw@gmail.com>
2020-04-12 16:32:34 +0700

    add aspirasi UI

--- application/views/aspirasi/index.php
+++ application/views/aspirasi/index.php
@@ -1,14 +1,44 @@
-<div class="col-sm-12">
-	<div class="row bs-wizard">
-		<div class="col-xs-2 col-xs-offset-1 bs-wizard-step active">
-			<div class="progress">
-				<div class="progress-bar">
-				
+<div class="box-centering">
+	<div class="page-wrapper">
+		<div class="page-content">
+			<div style="margin-bottom: 30px;">
+				<h1>Aspirasi</h1>
+			</div>
+			<div>
+				<p>Untuk memberikan masukan, saran, maupun kritikan, anda dapat menuliskan pada form di bawah ini. Wajib menuliskan identitas anda seperti NIM, Kategori, Organisasi, dan pesan atau aspirasi yang ingin di sampaikan.</p>
+			</div>
+			<div class="box-centering">
+				<div class="box-input">
+					<form action="" method="post">
+						<div class="form-group">
+							<label for="Nama-input">Nama</label>
+							<input type="text" class="form-control form-content" id="nama-input" placeholder="Nama Lengkap" required>
+						</div>
+						<div class="form-group">
+							<label for="NIM-input">Nomer Induk Mahasiswa (NIM)</label>
+							<input type="text" class="form-control form-content" id="NIM-input" maxlength="10" placeholder="1*********" required>
+						</div>
+						<div class="form-group">
+							<label for="Kategori-Input">Kategori</label>
+							<select class="form-control form-content" id="Kategori-input">
+								<option>1</option>
+								<option>2</option>
+								<option>3</option>
+								<option>4</option>
+								<option>5</option>
+							</select>
+						</div>
+						<div class="form-group">
+							<label for="aspirasi-input">Isi Aspirasi anda</label>
+							<textarea class="form-control text-area-input" id="aspirasi-input"></textarea>
+						</div>
+						<div class="box-centering">
+							<button type="button" class="btn btn-outline-warning btn-input">Clear</button>
+							<button type="button" class="btn btn-primary btn-input">Submit</button>
+						</div>
+					</form>
 				</div>
 			</div>
-			<span class="bs-wizard-dot">
-				<div class="fa fa-pencil-square-o"></div>
-			</span>
 		</div>
 	</div>
 </div>
