c49d108f81d3a3c978083944349a461a7ecaa3e8
aldiwira <aldiwira.aw@gmail.com>
2020-04-27 11:51:35 +0700

    add more library func

--- application/libraries/xls.php
+++ application/libraries/xls.php
@@ -8,7 +8,7 @@ use PhpOffice\PhpSpreadsheet\Writer\Xls as WriterXls;
 
 class xls
 {
-    public function export_xls_aspirasi($data)
+    public function export_xls_aspirasi($data, $type)
     {
         $spreadsheet = new Spreadsheet();
         //setting properties file
@@ -20,40 +20,67 @@ class xls
             ->setKeywords('office 2007 openxml php')
             ->setCategory('Test result file');
 
-        //setting nama kolom
-        $spreadsheet->setActiveSheetIndex(0)
-            ->setCellValue('A1', 'Nomor')
-            ->setCellValue('B1', 'Nama')
-            ->setCellValue('C1', 'Kategori')
-            ->setCellValue('D1', 'OKI')
-            ->setCellValue('E1', 'TUJUAN')
-            ->setCellValue('F1', 'Isi Aspirasi')
-            ->setCellValue('G1', 'Tanggal');
+        if ($type == 'aspirasi') {
+            //setting nama kolom
+            $spreadsheet->setActiveSheetIndex(0)
+                ->setCellValue('A1', 'Nomor')
+                ->setCellValue('B1', 'Nama')
+                ->setCellValue('C1', 'Kategori')
+                ->setCellValue('D1', 'OKI')
+                ->setCellValue('E1', 'TUJUAN')
+                ->setCellValue('F1', 'Isi Aspirasi')
+                ->setCellValue('G1', 'Tanggal');
 
-        //setting isi kolom
-        $kolom = 2;
-        $nomor = 1;
-        $field = 0;
-        // var_dump($data);
-        foreach ($data as $keys) {
-            foreach ($keys as $key) {
-                foreach ($key as $ke) {
-                    $spreadsheet->setActiveSheetIndex(0)
-                        ->setCellValue('A' . $kolom, $nomor)
-                        ->setCellValue('B' . $kolom, $ke["NAMA"])
-                        ->setCellValue('C' . $kolom, $ke["KAT_NAMA"])
-                        ->setCellValue('D' . $kolom, $ke["OKI_NAMA"])
-                        ->setCellValue('E' . $kolom, $ke["TUJUAN"])
-                        ->setCellValue('F' . $kolom, $ke["KONTEN"])
-                        ->setCellValue('G' . $kolom, $ke["DATE"]);
-                    $kolom++;
-                    $nomor++;
+            //setting isi kolom
+            $kolom = 2;
+            $nomor = 1;
+            // var_dump($data);
+            foreach ($data as $keys) {
+                foreach ($keys as $key) {
+                    foreach ($key as $ke) {
+                        $spreadsheet->setActiveSheetIndex(0)
+                            ->setCellValue('A' . $kolom, $nomor)
+                            ->setCellValue('B' . $kolom, $ke["NAMA"])
+                            ->setCellValue('C' . $kolom, $ke["KAT_NAMA"])
+                            ->setCellValue('D' . $kolom, $ke["OKI_NAMA"])
+                            ->setCellValue('E' . $kolom, $ke["TUJUAN"])
+                            ->setCellValue('F' . $kolom, $ke["KONTEN"])
+                            ->setCellValue('G' . $kolom, $ke["DATE"]);
+                        $kolom++;
+                        $nomor++;
+                    }
                 }
             }
+            $filename = "Aspirasi " . date("Y-m-d H:i:s");
+        } else if ($type == 'saran') {
+            //setting nama kolom
+            $spreadsheet->setActiveSheetIndex(0)
+                ->setCellValue('A1', 'Nomor')
+                ->setCellValue('B1', 'Nama')
+                ->setCellValue('C1', 'NIM')
+                ->setCellValue('D1', 'Saran')
+                ->setCellValue('E1', 'Tanggal');
+            $kolom = 2;
+            $nomor = 1;
+            foreach ($data as $keys) {
+                foreach ($keys as $key) {
+                    foreach ($key as $ke) {
+                        $spreadsheet->setActiveSheetIndex(0)
+                            ->setCellValue('A' . $kolom, $nomor)
+                            ->setCellValue('B' . $kolom, $ke["NAMA"])
+                            ->setCellValue('C' . $kolom, $ke["KAT_NAMA"])
+                            ->setCellValue('D' . $kolom, $ke["OKI_NAMA"])
+                            ->setCellValue('E' . $kolom, $ke["TUJUAN"]);
+                        $kolom++;
+                        $nomor++;
+                    }
+                }
+            }
+            $filename = "Saran " . date("Y-m-d H:i:s");
         }
+
+
         $writer = new WriterXls($spreadsheet);
-        //nama file xls
-        $filename = "Aspirasi" . date("Y-m-d H:i:s");;
 
         header('Content-Type: application/vnd.ms-excel');
         header('Content-Disposition: attachment;filename="' . $filename . '.xls"');
